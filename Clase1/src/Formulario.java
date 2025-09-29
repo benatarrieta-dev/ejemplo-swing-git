16021a66dfa2ee953ae273dcbc1a8f3aba148bbc
Beñat Arrieta <benat.arrieta@opendeusto.es>
2025-09-29 09:40:02 +0200

    Modified

--- Clase1/src/Formulario.java
+++ Clase1/src/Formulario.java
@@ -5,7 +5,11 @@ import java.awt.event.ActionListener;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 import javax.swing.JPanel;
+import javax.swing.JTabbedPane;
 
 public class Formulario extends JFrame{
 
@@ -18,6 +22,26 @@ public class Formulario extends JFrame{
 		setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
 		btnSalida = new JButton("SALIR");
 		pAbajo = new JPanel();
+		
+		
+		JMenuBar barraMenu = new JMenuBar();
+		setJMenuBar(barraMenu);
+		JMenu menuFile = new JMenu("File");
+		barraMenu.add(menuFile);
+		
+		JMenuItem itemMostras = new JMenuItem("Mostrar");
+		menuFile.add(itemMostras);
+		JMenuItem itemLimpiar = new JMenuItem("Limpiar");
+		menuFile.add(itemLimpiar);
+		JMenuItem itemSalir = new JMenuItem("Salir");
+		menuFile.add(itemSalir);
+		
+		
+		JTabbedPane panelPestanas = new JTabbedPane();
+		add(panelPestanas, BorderLayout.CENTER);
+		panelPestanas.addTab("Datos Personales", new JPanel());
+		panelPestanas.add("Información", new JPanel());
+		panelPestanas.add("Comentarios", new JPanel());
 		
 		
 		
